783ce002007e70c53e1499a3138071513cfe66d2
Pearl Marfatia <pearlm21x@gmail.com>
2023-09-11 19:36:52 +0100

    added links

--- Flaskattempt/app.py
+++ Flaskattempt/app.py
@@ -6,11 +6,35 @@ app.config['STATIC_FOLDER'] = 'static'
 def home():
     return render_template("index.html")
 
-# # New functions
-# @app.route("/about/")
-# def about():
-#     return render_template("about.html")
+# New functions
+@app.route("/aboutus/")
+def aboutus():
+    return render_template("aboutus.html")
 
-# @app.route("/contact/")
-# def contact():
-#     return render_template("contact.html")
+@app.route("/contactus/")
+def contactus():
+    return render_template("contactus.html")
+
+@app.route("/qa/")
+def qa():
+    return render_template("qa.html")
+
+@app.route("/week1/")
+def week1():
+    return render_template("week1.html")
+
+@app.route("/week2/")
+def week2():
+    return render_template("week2.html")
+
+@app.route("/week3/")
+def week3():
+    return render_template("week3.html")
+
+@app.route("/week4/")
+def week4():
+    return render_template("week4.html")
+
+@app.route("/week5/")
+def week5():
+    return render_template("week5.html")
